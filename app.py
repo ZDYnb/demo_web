e069aceebdbcb1d6d400faf751a524c18a5907d0
Eric <zdynb911@163.com>
2025-02-23 19:24:04 -0800

    suit for web pattern

--- app.py
+++ app.py
@@ -7,16 +7,26 @@ import requests
 from collections import deque
 import time
 
-# Streamlit Page Config
-st.set_page_config(page_title=" Real-Time Sensor Dashboard", layout="wide")
 
-# Connect to Firebase
-SERVICE_ACCOUNT_PATH = "lifealert-40baf-firebase-adminsdk-fbsvc-5bdb920efa.json"
-DATABASE_URL = "https://lifealert-40baf-default-rtdb.firebaseio.com/"
+# read Streamlit Secrets
+firebase_secrets = st.secrets["firebase"]
 
+# Firebase
 if not firebase_admin._apps:
-    cred = credentials.Certificate(SERVICE_ACCOUNT_PATH)
-    firebase_admin.initialize_app(cred, {"databaseURL": DATABASE_URL})
+    cred = credentials.Certificate({
+        "type": firebase_secrets["type"],
+        "project_id": firebase_secrets["project_id"],
+        "private_key_id": firebase_secrets["private_key_id"],
+        "private_key": firebase_secrets["private_key"].replace("\\n", "\n"),
+        "client_email": firebase_secrets["client_email"],
+        "client_id": firebase_secrets["client_id"],
+        "auth_uri": firebase_secrets["auth_uri"],
+        "token_uri": firebase_secrets["token_uri"],
+        "auth_provider_x509_cert_url": firebase_secrets["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": firebase_secrets["client_x509_cert_url"],
+    })
+    firebase_admin.initialize_app(cred, {"databaseURL": firebase_secrets["database_url"]})
+
 
 sensor_ref = db.reference("sensorData")
 
